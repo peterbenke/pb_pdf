7422dc2329d4a79a152c03e9a960e359dc2d814d
Muhammad Zohaib <malikzohaib404@gmail.com>
2022-11-13 23:02:33 +0100

    code cleanup and compatibility for TYPO3 v11.5.18

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,6 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Peter Benke',
 	'author_email' => 'info@typomotor.de',
     'state' => 'stable',
-	'clearCacheOnLoad' => 0,
     'version' => '10.4.0',
     'constraints' => [
         'depends' => [
